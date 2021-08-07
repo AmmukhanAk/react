fd0b63c47c4fed7202274433768f4e376edf90d6
AmmukhanAk <amerkhanak007@gmail.com>
2021-08-07 02:34:14 -0700

    updated version

---
+++ akapp/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import {Section} from './Component/Section';
+import "./Component/Section.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Section />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ akapp/src/Component/Section.jsx
@@ -0,0 +1,49 @@
+
+export const Section=()=>{
+    return(
+      <div>
+        <h1>786/92</h1>
+      <h2>Learning React with core language</h2>
+       
+       <section>
+         <article>
+           <h3>HTML</h3>
+       <ul>
+         <li>lalalalalalalalalalalalalalaal</li>
+         <li>hghjkdhgkhgjhjkghdkghkjhhjgjg</li>
+         <li>gfhgjhghghghgjhjghjgjhjghjhjhjg</li>
+         <li>kjgkjhgjdhfjhdkjhgkjhdjgkdjhgjh</li>
+       </ul>
+           
+         </article>
+       </section>
+             <h3>CSS</h3>
+             <ol>
+               <li>bfgjdhgkjdhgjhgihsdjhgj</li>
+               <li>ghidshgijshdgihsdihgidfhg</li>
+               <li>liuhgilhgiluhgiushigdiu</li>
+               <li>kjgdfhgljhdghkgildfhgjglighrg</li>
+             </ol>
+       <section>
+         <article>
+               <h3>js</h3>
+               <ul type="square">
+                 <li>bgjdhgkjgkjlkjgsdkjgkjdfgkjdkjg</li>
+                 <li>hfgsljglkjglkglkjfgjgljdfghjidhgjdh</li>
+                 <li>hlhdghliuhguisdhguihgihjstrhghl.dnhlkjrtg</li>
+                 <li>lghuh;oiggiudflhpsiuiyoiogdogy</li>
+               </ul>
+
+         </article>
+       </section>
+      
+       <section>
+         <article>
+
+
+         </article>
+       </section>
+      
+       </div>     
+    );
+}
